8dcdcf4a8708c1d75dd5774cd844602815f53658
unknown <khoivankarik@gmail.com>
2022-10-23 04:42:27 +0700

    Relational Source System Into Stage

---
+++ SQL Script/WWTemp.sql
@@ -0,0 +1,8 @@
+select * from KhachHang_Jade
+
+select * from Data_Flow
+
+select NgayCapNhat from Data_Flow where TenBang='jade'
+
+update Data_Flow set NgayCapNhat=GETDATE() where TenBang='jade'
+
